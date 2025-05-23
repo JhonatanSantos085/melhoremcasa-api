ae2fa83add265927984a971e5245d9837eee6d91
joao roberto <jrgarske5@gmail.com>
2025-05-23 19:17:56 -0300

    base atualizada

---
+++ data/base.sql
@@ -0,0 +1,134 @@
+DROP TABLE IF EXISTS usuario CASCADE;
+DROP TABLE IF EXISTS endereco CASCADE;
+DROP TABLE IF EXISTS paciente CASCADE;
+DROP TABLE IF EXISTS equipe CASCADE;
+DROP TABLE IF EXISTS consultoria CASCADE;
+DROP TABLE IF EXISTS veiculo CASCADE;
+DROP TABLE IF EXISTS corrida CASCADE;
+DROP TABLE IF EXISTS manutencao CASCADE;
+DROP TABLE IF EXISTS visita CASCADE;
+DROP TABLE IF EXISTS visita_usuario CASCADE;
+
+
+CREATE TABLE usuario (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR(150) NOT NULL UNIQUE,
+  nome VARCHAR(150) NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  roles VARCHAR(50) NOT NULL CHECK (roles IN ('ADMIN', 'USUARIO_TECNICO', 'USUARIO_ENFERMEIRO','USUARIO_MOTORISTA','USUARIO_MEDICO'))
+);
+
+
+CREATE TABLE endereco (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  rua VARCHAR(150) NOT NULL,
+  numero VARCHAR(10),
+  bairro VARCHAR(100),
+  cidade VARCHAR(100),
+  estado VARCHAR(2),
+  cep VARCHAR(9)
+);
+
+CREATE TABLE paciente (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  nome VARCHAR(150) NOT NULL,
+  idade INT NOT NULL,
+  data_nascimento DATE NOT NULL,
+  endereco_id BIGINT NOT NULL,
+  cpf VARCHAR(11),
+  cid VARCHAR(50),
+  telefone VARCHAR(12) NOT NULL,
+  status BOOLEAN NOT NULL,
+  CONSTRAINT fk_endereco FOREIGN KEY (endereco_id) REFERENCES endereco(id)
+);
+
+
+CREATE TABLE equipe (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  nome VARCHAR(150) NOT NULL
+);
+
+
+CREATE TABLE consultoria (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  paciente_id BIGINT NOT NULL,
+  usuario_id BIGINT NOT NULL,
+  unidade_saude VARCHAR(150) NOT NULL,
+  equipe_id BIGINT NOT NULL,
+  data_consultoria DATE NOT NULL,
+  solicitante VARCHAR(150) NOT NULL,
+  hora TIME NOT NULL,
+  status_baixa VARCHAR(50) NOT NULL
+  CHECK (status_baixa IN ('OBITO_HOSPITALAR', 'REINTERNACAO_HOSPITALAR', 'ILPI','SEM_REDE_APOIO','MUDANCA_ENDERECO','INTERNACAO_HOSPITALAR','CANCELAMENTO_SOLICITANTE', 'RECUSA_PACIENTE_FAMILIAR', 'NAO_LOCALIZADO', 'OBITO_DOMICILIAR'  )),
+  modalidade_visita VARCHAR(50) CHECK (modalidade_visita IN ('EMERGENCIA', 'UNIDADE_SAUDE', 'HOSPITAL')),
+
+  CONSTRAINT fk_consultoria_paciente FOREIGN KEY (paciente_id) REFERENCES paciente(id),
+  CONSTRAINT fk_consultoria_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+  CONSTRAINT fk_consultoria_equipe FOREIGN KEY (equipe_id) REFERENCES equipe(id)
+);
+
+CREATE TABLE veiculo (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  placa VARCHAR(10) NOT NULL UNIQUE,
+  modelo VARCHAR(100) NOT NULL,
+  km_rodado DECIMAL(10,2) NOT NULL,
+  km_atual DECIMAL(10,2) NOT NULL,
+  latitude DECIMAL(9,6),
+  longitude DECIMAL(9,6)
+);
+
+CREATE TABLE corrida (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  veiculo_id BIGINT NOT NULL,
+  usuario_id BIGINT NOT NULL,
+  km_inicial DECIMAL(10,2) NOT NULL,
+  km_final DECIMAL(10,2) NOT NULL,
+  horario_inicio TIME NOT NULL,
+  horario_final TIME NOT NULL,
+  posicao_inicial DECIMAL(9,6),
+  posicao_final DECIMAL(9,6),
+
+  CONSTRAINT fk_corrida_veiculo FOREIGN KEY (veiculo_id) REFERENCES veiculo(id),
+  CONSTRAINT fk_corrida_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+
+CREATE TABLE manutencao (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  corrida_id BIGINT NOT NULL,
+  solicitacao_manutencao VARCHAR(50) NOT NULL CHECK (solicitacao_manutencao IN (
+    'MANUTENCAO_PREVENTIVA',
+    'MANUTENCAO_CORRETIVA',
+    'LAVAGEM_EXTRA',
+    'REVISAO_CITYCAR'
+  )),
+  explicacao TEXT,
+
+  CONSTRAINT fk_manutencao_corrida FOREIGN KEY (corrida_id) REFERENCES corrida(id)
+);
+
+CREATE TABLE visita (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  paciente_id BIGINT NOT NULL,
+  corrida_id BIGINT NOT NULL,
+  equipe_id BIGINT NOT NULL,
+  agudizacao BOOLEAN NOT NULL,
+  inercorreu BOOLEAN NOT NULL,
+  status_visita VARCHAR(50) NOT NULL CHECK (status_visita IN ('CONSULTORIA_AGENDADA', 'CONSULTORIA_INICIADA', 'CONSULTORIA_ENCERRADA','CONSULTORIA_FINALIZADA','AGENDADA','INICIADA','REALIZADA','ENCERRADA')),
+  internacao_evitada BOOLEAN NOT NULL,
+  data_visita DATE NOT NULL,
+  via_admin VARCHAR(50) NOT NULL CHECK (via_admin IN ('VO', 'EV', 'IM','CVC','PICC','PORT_A_CATH','SC')),
+
+  CONSTRAINT fk_visita_paciente FOREIGN KEY (paciente_id) REFERENCES paciente(id),
+  CONSTRAINT fk_visita_corrida FOREIGN KEY (corrida_id) REFERENCES corrida(id),
+  CONSTRAINT fk_visita_equipe FOREIGN KEY (equipe_id) REFERENCES equipe(id)
+);
+
+
+CREATE TABLE visita_usuario (
+  visita_id BIGINT NOT NULL,
+  usuario_id BIGINT NOT NULL,
+
+  CONSTRAINT pk_visita_usuario PRIMARY KEY (visita_id, usuario_id),
+  CONSTRAINT fk_visita_usuario_visita FOREIGN KEY (visita_id) REFERENCES visita(id),
+  CONSTRAINT fk_visita_usuario_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
